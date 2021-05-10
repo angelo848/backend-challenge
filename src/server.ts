beac8579dcea2b4324ffc927fb33d962eed80879
Ângelo Salles <angelo_848@live.com>
2021-05-10 20:14:03 -0300

    started initial apollo server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log('hello world')
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`
+const resolvers = {
+  Query: {
+    hello: () => 'hello world'
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+server.listen().then(({ url }) => console.log(`Server started at ${url}`))
